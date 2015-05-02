5a4a3cedc6387213d7554efa360b15791a09e592
Syo Ikeda <suicaicoca@gmail.com>
2015-05-02 15:19:19 +0900

    Initial commit

---
+++ Himotoki/Himotoki.h
@@ -0,0 +1,19 @@
+//
+//  Himotoki.h
+//  Himotoki
+//
+//  Created by Syo Ikeda on 5/2/15.
+//  Copyright (c) 2015 Syo Ikeda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Himotoki.
+FOUNDATION_EXPORT double HimotokiVersionNumber;
+
+//! Project version string for Himotoki.
+FOUNDATION_EXPORT const unsigned char HimotokiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Himotoki/PublicHeader.h>
+
+
