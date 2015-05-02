88e4b611d3801df62959e149b184c265724ba6c1
Syo Ikeda <suicaicoca@gmail.com>
2015-05-02 20:52:53 +0900

    Test Int64 type

--- HimotokiTests/DecodableTest.swift
+++ HimotokiTests/DecodableTest.swift
@@ -16,6 +16,7 @@ class DecodableTest: XCTestCase {
             "first_name": "ABC",
             "last_name": "DEF",
             "age": 20,
+            "int64": NSNumber(longLong: INT64_MAX),
             "height": 175.9,
             "float": 32.1 as Float,
             "bool": true,
@@ -29,6 +30,7 @@ class DecodableTest: XCTestCase {
         XCTAssert(person?.firstName == "ABC")
         XCTAssert(person?.lastName == "DEF")
         XCTAssert(person?.age == 20)
+        XCTAssert(person?.int64 == INT64_MAX)
         XCTAssert(person?.height == 175.9)
         XCTAssert(person?.float == 32.1)
         XCTAssert(person?.bool == true)
@@ -48,6 +50,7 @@ struct Person: Decodable {
     let firstName: String
     let lastName: String
     let age: Int
+    let int64: Int64
     let height: Double
     let float: Float
     let bool: Bool
@@ -59,6 +62,7 @@ struct Person: Decodable {
             firstName: e.valueOrFail("first_name"),
             lastName: e.valueOrFail("last_name"),
             age: e.valueOrFail("age"),
+            int64: e.valueOrFail("int64"),
             height: e.valueOrFail("height"),
             float: e.valueOrFail("float"),
             bool: e.valueOrFail("bool"),
