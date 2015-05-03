9b14a07947e18704686b7692ee0d025439c678a5
Syo Ikeda <suicaicoca@gmail.com>
2015-05-03 17:34:24 +0900

    Add Mac targets

--- Himotoki/Himotoki.h
+++ Himotoki/Himotoki.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015 Syo Ikeda. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+@import Foundation;
 
 //! Project version number for Himotoki.
 FOUNDATION_EXPORT double HimotokiVersionNumber;
