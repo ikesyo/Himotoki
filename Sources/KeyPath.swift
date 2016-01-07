be51cec0fc72c44ce51d80b2e95fe32385cf4787
Syo Ikeda <suicaicoca@gmail.com>
2016-01-07 19:39:16 +0900

    Simplify StringLiteralConvertible implementation

--- Sources/KeyPath.swift
+++ Sources/KeyPath.swift
@@ -29,18 +29,15 @@ extension KeyPath: CustomStringConvertible {
 }
 
 extension KeyPath: StringLiteralConvertible {
-    public typealias UnicodeScalarLiteralType = StringLiteralType
-    public typealias ExtendedGraphemeClusterLiteralType = StringLiteralType
-
-    public init(unicodeScalarLiteral value: UnicodeScalarLiteralType) {
+    public init(unicodeScalarLiteral value: String) {
         self.init(value)
     }
 
-    public init(extendedGraphemeClusterLiteral value: ExtendedGraphemeClusterLiteralType) {
+    public init(extendedGraphemeClusterLiteral value: String) {
         self.init(value)
     }
 
-    public init(stringLiteral value: StringLiteralType) {
+    public init(stringLiteral value: String) {
         self.init(value)
     }
 }
