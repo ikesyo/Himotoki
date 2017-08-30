8bc7fe84efab6c7a75452c45a0f627996ca4391d
Sho Ikeda <suicaicoca@gmail.com>
2017-08-30 13:57:39 +0900

    Support decoding from `Data`

--- Sources/decode.swift
+++ Sources/decode.swift
@@ -6,9 +6,21 @@
 //  Copyright (c) 2015 Syo Ikeda. All rights reserved.
 //
 
+import struct Foundation.Data
+
+/// - Throws: DecodeError or an arbitrary ErrorType
+public func decodeValue<T: Decodable>(from data: Data) throws -> T {
+    return try T.decodeValue(from: data)
+}
+
 /// - Throws: DecodeError or an arbitrary ErrorType
 public func decodeValue<T: Decodable>(_ JSON: Any) throws -> T {
     return try T.decodeValue(JSON)
+}
+
+/// - Throws: DecodeError or an arbitrary ErrorType
+public func decodeValue<T: Decodable>(from data: Data, rootKeyPath: KeyPath) throws -> T {
+    return try T.decodeValue(from: data, rootKeyPath: rootKeyPath)
 }
 
 /// - Throws: DecodeError or an arbitrary ErrorType
@@ -17,8 +29,18 @@ public func decodeValue<T: Decodable>(_ JSON: Any, rootKeyPath: KeyPath) throws 
 }
 
 /// - Throws: DecodeError or an arbitrary ErrorType
+public func decodeArray<T: Decodable>(from data: Data) throws -> [T] {
+    return try [T].decode(from: data)
+}
+
+/// - Throws: DecodeError or an arbitrary ErrorType
 public func decodeArray<T: Decodable>(_ JSON: Any) throws -> [T] {
     return try [T].decode(JSON)
+}
+
+/// - Throws: DecodeError or an arbitrary ErrorType
+public func decodeArray<T: Decodable>(from data: Data, rootKeyPath: KeyPath) throws -> [T] {
+    return try [T].decode(from: data, rootKeyPath: rootKeyPath)
 }
 
 /// - Throws: DecodeError or an arbitrary ErrorType
@@ -27,8 +49,18 @@ public func decodeArray<T: Decodable>(_ JSON: Any, rootKeyPath: KeyPath) throws 
 }
 
 /// - Throws: DecodeError or an arbitrary ErrorType
+public func decodeDictionary<T: Decodable>(from data: Data) throws -> [String: T] {
+    return try [String: T].decode(from: data)
+}
+
+/// - Throws: DecodeError or an arbitrary ErrorType
 public func decodeDictionary<T: Decodable>(_ JSON: Any) throws -> [String: T] {
     return try [String: T].decode(JSON)
+}
+
+/// - Throws: DecodeError or an arbitrary ErrorType
+public func decodeDictionary<T: Decodable>(from data: Data, rootKeyPath: KeyPath) throws -> [String: T] {
+    return try [String: T].decode(from: data, rootKeyPath: rootKeyPath)
 }
 
 /// - Throws: DecodeError or an arbitrary ErrorType
